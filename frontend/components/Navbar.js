e5a6043a93620369f053e8531154a85caa1274ad
ikarrila <mjunposti@gmail.com>
2024-02-26 23:12:11 +0200

    Drop down for navigation

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Navbar.css';
 
 export default function Navbar() {
   return (
@@ -6,8 +7,21 @@ export default function Navbar() {
       <div style={{ display: "flex", gap: "1rem" }}>
         <div style={{ width: "50px", height: "50px", borderRadius: "50%", backgroundColor: "grey" }}></div>
         <a href="#">Home</a>
-        <a href="#">Our Services</a>
-        <a href="#">How it works</a>
+        <div className="dropdown">
+          <a href="#">Our Services</a>
+          <div className="dropdown-content">
+            <a href="#">Furnishing</a>
+            <a href="#">FAAS</a>
+            <a href="#">For Businesses</a>
+          </div>
+        </div>
+        <div className="dropdown">
+          <a href="#">How it works</a>
+          <div className="dropdown-content">
+            <a href="#">How it works</a>
+            <a href="#">About us</a>
+          </div>
+        </div>
         <a href="#">Portfolio</a>
         <a href="#">Contact</a>
         <a href="#">Blog</a>
