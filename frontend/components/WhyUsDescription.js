8dde9b94839ca9216172662d4199a7f168be0006
jmeskanen <johanna.meskanen@almare.fi>
2024-02-29 13:10:59 +0200

    Fixed alignsSelf to alignSelf

--- frontend/components/WhyUsDescription.js
+++ frontend/components/WhyUsDescription.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function WhyUsDescription() {
   return (
-    <div style={{ textAlign: 'center', alignsSelf: 'flex-start' }}>
+    <div style={{ textAlign: 'center', alignSelf: 'flex-start' }}>
       <h2 style={{ fontWeight: 'bold' }}>Why us?</h2>
       <p>Information about...</p>
       <button>Learn More</button>
