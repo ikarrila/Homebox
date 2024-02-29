373e8f09dcba3eca8e395f77e12f4b2469a062db
jmeskanen <johanna.meskanen@almare.fi>
2024-02-29 13:07:28 +0200

    Moved components to the left

--- frontend/components/WhyUsDescription.js
+++ frontend/components/WhyUsDescription.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function WhyUsDescription() {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: 'center', alignsSelf: 'flex-start' }}>
       <h2 style={{ fontWeight: 'bold' }}>Why us?</h2>
       <p>Information about...</p>
       <button>Learn More</button>
