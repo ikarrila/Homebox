b6229b315a9661490af1ee96857646ccdc83f709
jmeskanen <johanna.meskanen@almare.fi>
2024-02-29 13:02:14 +0200

    Created WhyUsDescription.js

---
+++ frontend/components/WhyUsDescription.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function WhyUsDescription() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2 style={{ fontWeight: 'bold' }}>Why us?</h2>
+      <p>Information about...</p>
+      <button>Learn More</button>
+    </div>
+  );
+}
