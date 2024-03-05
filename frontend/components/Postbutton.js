983bd18c3b718fa9acb2dc687b2f214eab5fde73
Mehis <vikkek@gmail.com>
2024-03-05 19:05:34 +0200

    working testbutton in costcalculatorform, able to post and get info from mongo

--- frontend/components/Postbutton.js
+++ frontend/components/Postbutton.js
@@ -3,15 +3,30 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 
-export default function Postbutton() {
+export default function Postbutton(content) {
     const consolelognewproduct = async () => {
         const res = await fetch('http://localhost:4000/products').then(res => res.json())
         console.log(res);
+    }
+    const postprod = async (content) => {
+        const product = {
+            name: 'Example Product',
+            price: 100
+        };
+        const res = await fetch('http://localhost:4000/products', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        }).then((response) => response.json())
+            .then((json) => console.log(json));
     }
 
     return (
         <div >
             <button class='button' onClick={() => consolelognewproduct()}>console.logs the content</button>
+            <button class='button' onClick={() => postprod(content)}>POSTING TEST!! :D</button>
             <div>
                 test div, button above console.logs stuff
             </div>
