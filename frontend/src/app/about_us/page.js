e94c9d0a8c63c7d093f0f79dca3b5c008d615ed1
Mehis <vikkek@gmail.com>
2024-03-08 11:10:38 +0200

    routet how_it_works ja about_us

---
+++ frontend/src/app/about_us/page.js
@@ -0,0 +1,4 @@
+export default function about_us() {
+    return (<h1>About us</h1>
+    );
+}
---
+++ frontend/src/app/how_it_works/page.js
@@ -0,0 +1,4 @@
+export default function how_it_works() {
+    return (<h1>How it works</h1>
+    );
+}
