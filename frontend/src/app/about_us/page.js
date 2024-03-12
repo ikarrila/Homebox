4bad1997a1211a46985a3bd810e3fd3301952945
Saana Hänninen <saana.hanninen@tuni.fi>
2024-03-12 10:28:30 +0200

    edit placeholder page contents

--- frontend/src/app/about_us/page.js
+++ frontend/src/app/about_us/page.js
@@ -1,9 +1,10 @@
-import WhyUsDescription from "../../../components/WhyUsDescription";
+import AboutUs from "../../../components/AboutUs";
 
 export default function about_us() {
     return (
         <div>
             <h1>About us</h1>
+            <AboutUs></AboutUs>
         </div>
     );
 }
