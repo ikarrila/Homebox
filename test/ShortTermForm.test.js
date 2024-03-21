abdf22c57f3c2374b7c0fb22a443f16177c22fce
kaisakauhanen <kaisa.kauhanen@tuni.fi>
2024-03-21 14:02:10 +0200

    ShortTermForm unit tests modified

--- test/ShortTermForm.test.js
+++ test/ShortTermForm.test.js
@@ -8,15 +8,26 @@ describe('ShortTermForm', () => {
     render(<ShortTermForm />);
   });
 
+  test('renders correct heading', () => {
+    render(<ShortTermForm />);
+    expect(screen.getByText('Short term rental services')).toBeInTheDocument();
+  });
+
   test('renders input field', () => {
     render(<ShortTermForm />);
     const inputElement = screen.getByTestId('input');
     expect(inputElement).toBeInTheDocument();
   });
 
-  test('renders select dropdown', () => {
+  test('renders input fields and select dropdown', () => {
     render(<ShortTermForm />);
+    const inputElement = screen.getByTestId('input');
     const selectElement = screen.getByTestId('select');
+    const nameInputElement = screen.getByLabelText('Name:');
+    const emailInputElement = screen.getByLabelText('Email:');
+    expect(inputElement).toBeInTheDocument();
     expect(selectElement).toBeInTheDocument();
+    expect(nameInputElement).toBeInTheDocument();
+    expect(emailInputElement).toBeInTheDocument();
   });
 });
