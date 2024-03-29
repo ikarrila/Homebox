948dfea56e33cb314fde673d48dde4ab83fa5f9a
ikarrila <mjunposti@gmail.com>
2024-03-29 10:46:19 +0200

    FAQ content updated

--- frontend/components/AboutUs.js
+++ frontend/components/AboutUs.js
@@ -4,10 +4,15 @@ import '/styles/styles.css';
 export default function AboutUs() {
     return (
         <div className='container col'>
-            <h1>About us</h1>
+            <h1>Bringing a sense of instant comfort and familiarity to make you feel right at home. </h1>
             <div className='section col'>
                 <h2>Our Story</h2>
-                <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nemo similique, rerum culpa molestiae necessitatibus non magni itaque veniam nam adipisci vel animi sit aliquid eaque assumenda maiores asperiores nobis numquam! Ex non, doloribus ipsa et culpa tenetur ad, impedit quo soluta totam eligendi porro quae? Adipisci labore modi quidem voluptatem non hic voluptas quas rerum voluptates ipsam cumque impedit, incidunt repellat ea consectetur nihil corporis ratione ullam enim quo eaque. Soluta ipsa officia molestias. Nobis, esse aut! Ea aut dolore explicabo consequuntur est dolor quaerat suscipit qui ad quia maiores eos vel, aliquid facilis laborum? Minima iste neque odio.</p>
+                <p>
+
+                    Karen Justice founded Northwest Realtors and Brokers in 2008 after her own homebuying experience. Even though she had her Realtor license, being on the \“other side,\” so to speak gave her better insight into her clients' experiences. She had a better understanding of the confusion, frustration, and stress that overwhelmed homebuyers and sellers experienced and decided it was time to create more positive experiences.
+
+                    At Northwest Realtors and Brokers, we go beyond helping you buy or sell a house. We sit down with you to help you understand and complete each step of the process, from understanding pre-approvals through closing so you are confident, empowered, and excited, rather than overwhelmed and worried. Our team is here when you need us, whether to ask a quick question or quell your concerns.
+                </p>
             </div>
 
         </div>
