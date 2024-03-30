8813a61ba4f020b4dd90c7da0124d8138eb21676
ikarrila <mjunposti@gmail.com>
2024-03-30 23:39:03 +0200

    Privacy policy page updated

---
+++ frontend/components/PrivacyPolicyContent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Kuva from "../../pictures/toa-heftiba-FV3GConVSss-unsplash.jpg";
+import Image from 'next/image';
+import Link from 'next/link';
+import '/styles/styles.css';
+
+export default function PrivacyPolicyContent() {
+  return (
+
+    <div className='container row'>
+      <div className='section row'>
+        <div className='left'>
+          <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use Kiki's Homebox if you do not agree to take all of the terms and conditions stated on this page.</p>
+
+          <h3><strong>Cookies</strong></h3>
+
+          <p>We employ the use of cookies. By accessing Kiki's Homebox, you agreed to use cookies in agreement with the Kiki's Homebox's Privacy Policy.</p>
+
+          <p>Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
+
+          <h3><strong>License</strong></h3>
+
+          <p>Unless otherwise stated, Kiki's Homebox and/or its licensors own the intellectual property rights for all material on Kiki's Homebox. All intellectual property rights are reserved. You may access this from Kiki's Homebox for your own personal use subjected to restrictions set in these terms and conditions.</p></div>
+
+        <div>
+
+        </div>
+      </div>
+    </div >
+  );
+}
--- frontend/src/app/terms/privacypolicy/page.js
+++ frontend/src/app/terms/privacypolicy/page.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PrivacyPolicyContent from "../../../../components/PrivacyPolicyContent";
 
 export default function PrivacyPolicy() {
     return (
         <div>
             <h1>Privacy Policy</h1>
             {/* Your privacy policy content here */}
+            <PrivacyPolicyContent />
         </div>
     );
 }
