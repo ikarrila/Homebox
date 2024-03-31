b3cdba2d213a466c63d2e62df68fb597e262cae0
ikarrila <mjunposti@gmail.com>
2024-03-31 19:44:53 +0300

    Aligned tests to match the current state

--- test/Portfolio.test.js
+++ test/Portfolio.test.js
@@ -10,12 +10,12 @@ describe('Portfolio', () => {
 
   test('renders correct heading', () => {
     render(<Portfolio />);
-    expect(screen.getByText('Portfolio')).toBeInTheDocument();
+    expect(screen.getByText('See the spaces we furnished')).toBeInTheDocument();
   });
 
   test('renders correct number of images', () => {
     render(<Portfolio />);
     const imageElements = screen.getAllByRole('img');
-    expect(imageElements).toHaveLength(6);
+    expect(imageElements).toHaveLength(7);
   });
 });
