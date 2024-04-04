637c9f4615cffac55f73c5dcd683c59a74c6a2c8
jmeskanen <johanna.meskanen@almare.fi>
2024-04-04 15:22:24 +0300

    Created components LongTermFaas, ShortTermFaas, Sustainability, Faas and RentingWithUs. Added all of these components to page.js for Rental Services landing page.

---
+++ frontend/components/RentingWithUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '/styles/styles.css';
+
+
+export default function RentingWithUs() {
+    return (
+        <div className="section col" style={{ textAlign: 'center' }}>
+            <h2 className="font-bold">Renting With Us</h2>
+        </div>
+    );
+}
