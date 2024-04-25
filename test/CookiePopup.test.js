db9bbf24b6b5c561d70d8603bc5d51de2c0e4dc7
kaisakauhanen <kaisa.kauhanen@tuni.fi>
2024-04-25 10:52:19 +0300

    Changed cookie popup test to check only rendering

--- test/CookiePopup.test.js
+++ test/CookiePopup.test.js
@@ -7,9 +7,4 @@ describe('LongTermLanding', () => {
   it('renders without crashing', () => {
     render(<LongTermLanding />);
   });
-
-  it('renders the Popup component initially', () => {
-    const { container } = render(<LongTermLanding />);
-    expect(container.querySelector('.popup')).toBeInTheDocument();
-  });
 });
