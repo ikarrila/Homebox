6116b7af0c3d5d2c1c841d1555ca7171488d7c1f
kaisakauhanen <kaisa.kauhanen@tuni.fi>
2024-04-25 10:50:09 +0300

    Jest-dom library added to cookie popup test

--- test/CookiePopup.test.js
+++ test/CookiePopup.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import LongTermLanding from '../frontend/components/LongTermLanding';
 
 describe('LongTermLanding', () => {
