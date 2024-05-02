67a2f449cc110ca2212643e4a1695444132bf1cb
Saana Hänninen <saana.hanninen@tuni.fi>
2024-05-02 15:32:04 +0300

    use RentingWithUs instead of WhyUs component

--- frontend/components/RentingWithUs.js
+++ frontend/components/RentingWithUs.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 import '/styles/styles.css';
 
 
 export default function RentingWithUs() {
     return (
-        <div className="section col center-text">
+        <div className="section col center-text accent">
             <h2 className="section">Renting With Us</h2>
             <p className="container">
 
-                KEJA works in the gap between decorators and suppliers. We make furnishing easy and efficient with a comprehensive furnishing solution. We work with carefully chosen partners who share our commitment to ease and excellence. Above all, whether you’re relocating to a new apartment or simply want to refresh your current decor, we can furnish it for you – quickly and beautifully. Learn more about KEJA here.
+                KEJA works in the gap between decorators and suppliers. We make furnishing easy and efficient with a comprehensive furnishing solution. We work with carefully chosen partners who share our commitment to ease and excellence. Above all, whether you’re relocating to a new apartment or simply want to refresh your current decor, we can furnish it for you – quickly and beautifully.
             </p>
+            <Link href="/how_it_works">
+                <button className='btn-tertiary'>Learn More</button>
+            </Link>
         </div>
     );
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import Description from '../../components/Description';
-import WhyUsDescription from "../../components/WhyUsDescription";
 import CustomerReviews from "../../components/CustomerReviews";
+import RentingWithUs from "../../components/RentingWithUs";
 
 export default function Home() {
   return (
     <div>
       <Description />
       <CustomerReviews />
-      <WhyUsDescription />
+      <RentingWithUs />
     </div>
   );
 }
