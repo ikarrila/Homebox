71c9e739316d888935ce0f3b6236f491adf8679b
Inka <inka.ratia@tuni.fi>
2024-05-03 12:27:55 +0300

    poistetaan contactform otsikko.

--- frontend/src/app/contact/page.js
+++ frontend/src/app/contact/page.js
@@ -3,9 +3,6 @@ import ContactForm from "../../../components/ContactForm";
 function App() {
     return (
         <div>
-            <header style={{ padding: '10px', backgroundColor: '#f5f5f5', marginBottom: '20px' }}>
-                <h1>Contact form (long-term only?)</h1>
-            </header>
             <ContactForm />
         </div>
     );
