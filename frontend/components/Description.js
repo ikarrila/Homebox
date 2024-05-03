478ec248d4b2764bdb65a54ede9e09c008974bf8
Inka <inka.ratia@tuni.fi>
2024-05-03 11:14:52 +0300

    toppausta linkkeihin ja tekstimuutos.

--- frontend/components/Description.js
+++ frontend/components/Description.js
@@ -2,28 +2,35 @@ import React from 'react';
 import Kuva from "../../pictures/toa-heftiba-FV3GConVSss-unsplash.jpg";
 import Image from 'next/image';
 import Link from 'next/link';
+import '/styles/styles.css';
 
 export default function Description() {
   return (
     <div className='container row'>
       <div className='section row'>
-        <div className='left'> <h1>Efficient and effective interior furnishing solutions</h1>
-          <p>
-            Our comprehensive furnishing solutions offer a tailored approach in enhancing the functionality and aesthetics of your home.</p>
-          <div className="link-container">
+        <div className='left'> 
+          <h1>
+            Efficient and effective interior furnishing solutions
+          </h1>
+          <p style={{paddingTop:'15px'}}>
+              Our comprehensive furnishing solutions offer a tailored approach in enhancing the functionality and aesthetics of your home.
+          </p>
+          <div className="link-container" style={{paddingTop:'30px'}}>
             <Link href="" className='btn-primary'>
               Furnishing Services
             </Link>
-            <Link href="/FAAS/rental_services" className='btn-secondary'>Furniture Rental</Link>
+            <Link href="/FAAS/rental_services" className='btn-secondary'>
+              Rental Services
+            </Link>
           </div></div>
-        <div className='right'>        <div>
+        <div className='right'>        
           <Image
             src={Kuva}
             alt="Filler picture"
             className='image-container'
             width='1000px'
           />
-        </div></div>
+        </div>
       </div>
     </div >
   );
